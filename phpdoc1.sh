2f4a9b4c5d5f087486280a69e4102964b97b3112
Jason Hinkle <jason@eventric.com>
2012-08-20 21:10:02 -0500

    update documentation only

--- phpdoc1.sh
+++ phpdoc1.sh
@@ -1,4 +1,13 @@
-# generate documentation with phpdocumentor v1.  for v2, phpdoc.dist.xml is used instead
+# PHREEZE DOCUMENTATION GENERATOR
+# This script is for user with phpdoc v1 only.
+# If you have phpdoc v2 installed then use phpdoc2.sh
+# instead of this script.
+#
+# Prerequisites:
+#	sudo pear install PhpDocumentor-1.4.3
+#
+# Configuration settings are in the command line below:
+
 rm -rf docs
 phpdoc -d libs/verysimple/ -f /usr/lib/php/pear/PEAR.php,libs/savant/Savant3.php,libs/smarty/Smarty.class.php,libs/smarty/Smarty_Compiler.class.php,libs/phpmailer/class.phpmailer.php -i .*,class.*.php -t docs -dn verysimple--Phreeze -dc VerySimple -o HTML:frames:DOM/earthli -ti "Phreeze API Documentation"
 chmod -R 755 docs
--- phpdoc2.sh
+++ phpdoc2.sh
@@ -1,3 +1,14 @@
+# PHREEZE DOCUMENTATION GENERATOR
+# This script is for user with phpdoc v2 only.
+# If you have phpdoc v1 installed then use phpdoc1.sh
+# instead of this script.
+#
+# Prerequisites:
+# 	sudo pear channel-discover pear.phpdoc.org
+#	sudo pear install phpdoc/phpDocumentor-alpha 
+#
+# Configuration settings are located in phpdoc.dist.xml
+
 rm -rfd docs
 phpdoc
 chmod -R 755 docs
