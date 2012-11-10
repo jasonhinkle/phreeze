a36a36f42afd71edc2d8552435f6144d22eb2b34
Jason Hinkle <jason@eventric.com>
2012-11-10 16:10:48 -0600

    documentation update

--- libs/verysimple/Authentication/IAuthenticatable.php
+++ libs/verysimple/Authentication/IAuthenticatable.php
@@ -11,8 +11,29 @@
  */
 interface IAuthenticatable
 {
+	/**
+	 * Return true if the user is anonymous, meaning they have not
+	 * yet authenticated
+	 * @return bool
+	 */
 	public function IsAnonymous();
+	
+	/**
+	 * Verify if the IAuthenticable object has the requested permission
+	 * and return either true or false
+	 * @param variant $permission
+	 * @return bool
+	 */
 	public function IsAuthorized($permission);
+	
+	/**
+	 * Verify the login information and, if correct, this object will
+	 * populate itself and return a reference to $this.  If login fails
+	 * then return false/null.
+	 * @param string $username
+	 * @param string $password
+	 * @return IAuthenticatable or false/null
+	 */
 	public function Login($username,$password);
 }
 
