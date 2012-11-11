f198f0a0a4bb07fdfcaca1392725f78f17b253f7
Jason Hinkle <jason@eventric.com>
2012-11-11 03:46:31 -0600

    added Laravel/Blade Render Engine

---
+++ libs/laravel/paths.php
@@ -0,0 +1,30 @@
+<?php 
+
+/**
+ * A global path helper function.
+ *
+ * <code>
+ *     $storage = path('storage');
+ * </code>
+ *
+ * @param  string  $path
+ * @return string
+ */
+function path($path)
+{
+	return $GLOBALS['laravel_paths'][$path];
+}
+
+/**
+ * A global path setter function.
+ *
+ * @param  string  $path
+ * @param  string  $value
+ * @return void
+ */
+function set_path($path, $value)
+{
+	$GLOBALS['laravel_paths'][$path] = $value;
+}
+
+?>
