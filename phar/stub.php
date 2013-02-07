e8636007ae6efceca9df4c02575faf4a2d64d828
Jason Hinkle <jason@eventric.com>
2013-02-06 22:36:42 -0600

    added phar builder

---
+++ phar/stub.php
@@ -0,0 +1,16 @@
+<?php
+/**
+ * Stub file for phreeze.phar
+ * if included directly (ex include 'phreeze.phar') then this
+ * code will execute.  It will not execute if the .phar is only
+ * part of the include path.
+ * 
+ * @TODO include base classes of framework for convenience..?
+ */
+
+// $phar = 'phar://' . __FILE__;
+// require_once $phar . '/verysimple/HTTP/RequestUtil.php';
+// require_once $phar . '/verysimple/Phreeze/Phreezer.php';
+// ...
+
+__HALT_COMPILER();
