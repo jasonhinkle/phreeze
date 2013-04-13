b400bc9abcf47331816c3519952b4e84c9b9d0d5
Jason Hinkle <jason@eventric.com>
2013-04-13 02:09:41 -0500

    fixed parse errors in convert_html_to_text

--- libs/util/html2text.php
+++ libs/util/html2text.php
@@ -24,21 +24,21 @@
  * @return the HTML converted, as best as possible, to text
  */
 function convert_html_to_text($html) {
-﻿  $html = fix_newlines($html);
+	$html = fix_newlines($html);
 
-﻿  $doc = new DOMDocument();
-﻿  if (!$doc->loadHTML($html))
-﻿  ﻿  throw new Html2TextException("Could not load HTML - badly formed?", $html);
+	$doc = new DOMDocument();
+	if (!$doc->loadHTML($html))
+		throw new Html2TextException("Could not load HTML - badly formed?", $html);
 
-﻿  $output = iterate_over_node($doc);
+	$output = iterate_over_node($doc);
 
-﻿  // remove leading and trailing spaces on each line
-﻿  $output = preg_replace("/[ \t]*\n[ \t]*/im", "\n", $output);
+	// remove leading and trailing spaces on each line
+	$output = preg_replace("/[ \t]*\n[ \t]*/im", "\n", $output);
 
-﻿  // remove leading and trailing whitespace
-﻿  $output = trim($output);
+	// remove leading and trailing whitespace
+	$output = trim($output);
 
-﻿  return $output;
+	return $output;
 }
 
 /**
@@ -50,176 +50,176 @@ function convert_html_to_text($html) {
  * @return the fixed text
  */
 function fix_newlines($text) {
-﻿  // replace \r\n to \n
-﻿  $text = str_replace("\r\n", "\n", $text);
-﻿  // remove \rs
-﻿  $text = str_replace("\r", "\n", $text);
+	// replace \r\n to \n
+	$text = str_replace("\r\n", "\n", $text);
+	// remove \rs
+	$text = str_replace("\r", "\n", $text);
 
-﻿  return $text;
+	return $text;
 }
 
 function next_child_name($node) {
-﻿  // get the next child
-﻿  $nextNode = $node->nextSibling;
-﻿  while ($nextNode != null) {
-﻿  ﻿  if ($nextNode instanceof DOMElement) {
-﻿  ﻿  ﻿  break;
-﻿  ﻿  }
-﻿  ﻿  $nextNode = $nextNode->nextSibling;
-﻿  }
-﻿  $nextName = null;
-﻿  if ($nextNode instanceof DOMElement && $nextNode != null) {
-﻿  ﻿  $nextName = strtolower($nextNode->nodeName);
-﻿  }
+	// get the next child
+	$nextNode = $node->nextSibling;
+	while ($nextNode != null) {
+		if ($nextNode instanceof DOMElement) {
+			break;
+		}
+		$nextNode = $nextNode->nextSibling;
+	}
+	$nextName = null;
+	if ($nextNode instanceof DOMElement && $nextNode != null) {
+		$nextName = strtolower($nextNode->nodeName);
+	}
 
-﻿  return $nextName;
+	return $nextName;
 }
 function prev_child_name($node) {
-﻿  // get the previous child
-﻿  $nextNode = $node->previousSibling;
-﻿  while ($nextNode != null) {
-﻿  ﻿  if ($nextNode instanceof DOMElement) {
-﻿  ﻿  ﻿  break;
-﻿  ﻿  }
-﻿  ﻿  $nextNode = $nextNode->previousSibling;
-﻿  }
-﻿  $nextName = null;
-﻿  if ($nextNode instanceof DOMElement && $nextNode != null) {
-﻿  ﻿  $nextName = strtolower($nextNode->nodeName);
-﻿  }
+	// get the previous child
+	$nextNode = $node->previousSibling;
+	while ($nextNode != null) {
+		if ($nextNode instanceof DOMElement) {
+			break;
+		}
+		$nextNode = $nextNode->previousSibling;
+	}
+	$nextName = null;
+	if ($nextNode instanceof DOMElement && $nextNode != null) {
+		$nextName = strtolower($nextNode->nodeName);
+	}
 
-﻿  return $nextName;
+	return $nextName;
 }
 
 function iterate_over_node($node) {
-﻿  if ($node instanceof DOMText) {
-﻿  ﻿  return preg_replace("/\\s+/im", " ", $node->wholeText);
-﻿  }
-﻿  if ($node instanceof DOMDocumentType) {
-﻿  ﻿  // ignore
-﻿  ﻿  return "";
-﻿  }
+	if ($node instanceof DOMText) {
+		return preg_replace("/\\s+/im", " ", $node->wholeText);
+	}
+	if ($node instanceof DOMDocumentType) {
+		// ignore
+		return "";
+	}
 
-﻿  $nextName = next_child_name($node);
-﻿  $prevName = prev_child_name($node);
+	$nextName = next_child_name($node);
+	$prevName = prev_child_name($node);
 
-﻿  $name = strtolower($node->nodeName);
+	$name = strtolower($node->nodeName);
 
-﻿  // start whitespace
-﻿  switch ($name) {
-﻿  ﻿  case "hr":
-﻿  ﻿  ﻿  return "------\n";
+	// start whitespace
+	switch ($name) {
+		case "hr":
+			return "------\n";
 
-﻿  ﻿  case "style":
-﻿  ﻿  case "head":
-﻿  ﻿  case "title":
-﻿  ﻿  case "meta":
-﻿  ﻿  case "script":
-﻿  ﻿  ﻿  // ignore these tags
-﻿  ﻿  ﻿  return "";
+		case "style":
+		case "head":
+		case "title":
+		case "meta":
+		case "script":
+			// ignore these tags
+			return "";
 
-﻿  ﻿  case "h1":
-﻿  ﻿  case "h2":
-﻿  ﻿  case "h3":
-﻿  ﻿  case "h4":
-﻿  ﻿  case "h5":
-﻿  ﻿  case "h6":
-﻿  ﻿  ﻿  // add two newlines
-﻿  ﻿  ﻿  $output = "\n";
-﻿  ﻿  ﻿  break;
+		case "h1":
+		case "h2":
+		case "h3":
+		case "h4":
+		case "h5":
+		case "h6":
+			// add two newlines
+			$output = "\n";
+			break;
 
-﻿  ﻿  case "p":
-﻿  ﻿  case "div":
-﻿  ﻿  ﻿  // add one line
-﻿  ﻿  ﻿  $output = "\n";
-﻿  ﻿  ﻿  break;
+		case "p":
+		case "div":
+			// add one line
+			$output = "\n";
+			break;
 
-﻿  ﻿  default:
-﻿  ﻿  ﻿  // print out contents of unknown tags
-﻿  ﻿  ﻿  $output = "";
-﻿  ﻿  ﻿  break;
-﻿  }
+		default:
+			// print out contents of unknown tags
+			$output = "";
+			break;
+	}
 
-﻿  // debug
-﻿  //$output .= "[$name,$nextName]";
+	// debug
+	//$output .= "[$name,$nextName]";
 
-﻿  for ($i = 0; $i < $node->childNodes->length; $i++) {
-﻿  ﻿  $n = $node->childNodes->item($i);
+	for ($i = 0; $i < $node->childNodes->length; $i++) {
+		$n = $node->childNodes->item($i);
 
-﻿  ﻿  $text = iterate_over_node($n);
+		$text = iterate_over_node($n);
 
-﻿  ﻿  $output .= $text;
-﻿  }
+		$output .= $text;
+	}
 
-﻿  // end whitespace
-﻿  switch ($name) {
-﻿  ﻿  case "style":
-﻿  ﻿  case "head":
-﻿  ﻿  case "title":
-﻿  ﻿  case "meta":
-﻿  ﻿  case "script":
-﻿  ﻿  ﻿  // ignore these tags
-﻿  ﻿  ﻿  return "";
+	// end whitespace
+	switch ($name) {
+		case "style":
+		case "head":
+		case "title":
+		case "meta":
+		case "script":
+			// ignore these tags
+			return "";
 
-﻿  ﻿  case "h1":
-﻿  ﻿  case "h2":
-﻿  ﻿  case "h3":
-﻿  ﻿  case "h4":
-﻿  ﻿  case "h5":
-﻿  ﻿  case "h6":
-﻿  ﻿  ﻿  $output .= "\n";
-﻿  ﻿  ﻿  break;
+		case "h1":
+		case "h2":
+		case "h3":
+		case "h4":
+		case "h5":
+		case "h6":
+			$output .= "\n";
+			break;
 
-﻿  ﻿  case "p":
-﻿  ﻿  case "br":
-﻿  ﻿  ﻿  // add one line
-﻿  ﻿  ﻿  if ($nextName != "div")
-﻿  ﻿  ﻿  ﻿  $output .= "\n";
-﻿  ﻿  ﻿  break;
+		case "p":
+		case "br":
+			// add one line
+			if ($nextName != "div")
+				$output .= "\n";
+			break;
 
-﻿  ﻿  case "div":
-﻿  ﻿  ﻿  // add one line only if the next child isn't a div
-﻿  ﻿  ﻿  if ($nextName != "div" && $nextName != null)
-﻿  ﻿  ﻿  ﻿  $output .= "\n";
-﻿  ﻿  ﻿  break;
+		case "div":
+			// add one line only if the next child isn't a div
+			if ($nextName != "div" && $nextName != null)
+				$output .= "\n";
+			break;
 
-﻿  ﻿  case "a":
-﻿  ﻿  ﻿  // links are returned in [text](link) format
-﻿  ﻿  ﻿  $href = $node->getAttribute("href");
-﻿  ﻿  ﻿  if ($href == null) {
-﻿  ﻿  ﻿  ﻿  // it doesn't link anywhere
-﻿  ﻿  ﻿  ﻿  if ($node->getAttribute("name") != null) {
-﻿  ﻿  ﻿  ﻿  ﻿  $output = "[$output]";
-﻿  ﻿  ﻿  ﻿  }
-﻿  ﻿  ﻿  } else {
-﻿  ﻿  ﻿  ﻿  if ($href == $output) {
-﻿  ﻿  ﻿  ﻿  ﻿  // link to the same address: just use link
-﻿  ﻿  ﻿  ﻿  ﻿  $output;
-﻿  ﻿  ﻿  ﻿  } else {
-﻿  ﻿  ﻿  ﻿  ﻿  // replace it
-﻿  ﻿  ﻿  ﻿  ﻿  $output = "[$output]($href)";
-﻿  ﻿  ﻿  ﻿  }
-﻿  ﻿  ﻿  }
+		case "a":
+			// links are returned in [text](link) format
+			$href = $node->getAttribute("href");
+			if ($href == null) {
+				// it doesn't link anywhere
+				if ($node->getAttribute("name") != null) {
+					$output = "[$output]";
+				}
+			} else {
+				if ($href == $output) {
+					// link to the same address: just use link
+					$output;
+				} else {
+					// replace it
+					$output = "[$output]($href)";
+				}
+			}
 
-﻿  ﻿  ﻿  // does the next node require additional whitespace?
-﻿  ﻿  ﻿  switch ($nextName) {
-﻿  ﻿  ﻿  ﻿  case "h1": case "h2": case "h3": case "h4": case "h5": case "h6":
-﻿  ﻿  ﻿  ﻿  ﻿  $output .= "\n";
-﻿  ﻿  ﻿  ﻿  ﻿  break;
-﻿  ﻿  ﻿  }
+			// does the next node require additional whitespace?
+			switch ($nextName) {
+				case "h1": case "h2": case "h3": case "h4": case "h5": case "h6":
+					$output .= "\n";
+					break;
+			}
 
-﻿  ﻿  default:
-﻿  ﻿  ﻿  // do nothing
-﻿  }
+		default:
+			// do nothing
+	}
 
-﻿  return $output;
+	return $output;
 }
 
 class Html2TextException extends Exception {
-﻿  var $more_info;
+	var $more_info;
 
-﻿  public function __construct($message = "", $more_info = "") {
-﻿  ﻿  parent::__construct($message);
-﻿  ﻿  $this->more_info = $more_info;
-﻿  }
+	public function __construct($message = "", $more_info = "") {
+		parent::__construct($message);
+		$this->more_info = $more_info;
+	}
 }
