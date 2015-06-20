cb3e42099755a05c5d4c5a8fabee77afa4ba8d0d
Jason Hinkle <jason@eventric.com>
2015-06-20 00:09:55 -0500

    fixed missing class in .phar per issue #199 #196 #169

--- builder/code/phreeze.phpunit/test/lib/VerboseTestResultPrinter.php
+++ builder/code/phreeze.phpunit/test/lib/VerboseTestResultPrinter.php
@@ -1,11 +1,13 @@
 <?php
 /**
- * @package test::lib
+ * @package verysimple::phreeze
  */
 
 /**
  * This test listener outputs the name as each test is running
  * instead of simply printing progress dots as the tests run
+ * @version 1.1
+ * @author Jason Hinkle <verysimple.com>
  */
 class VerboseTestResultPrinter extends PHPUnit_TextUI_ResultPrinter
 {
@@ -35,6 +37,17 @@ class VerboseTestResultPrinter extends PHPUnit_TextUI_ResultPrinter
 	public function endTest(PHPUnit_Framework_Test $test, $time)
 	{
 		
+		// copied from parent:endTest()
+		if ($test instanceof PHPUnit_Framework_TestCase) {
+			$this->numAssertions += $test->getNumAssertions();
+		}
+		else if ($test instanceof PHPUnit_Extensions_PhptTestCase) {
+			$this->numAssertions++;
+		}
+		$this->lastTestFailed = false;
+		
+		
+		// custom printing code
 		if (get_class($test) == 'PHPUnit_Framework_TestSuite') {
 			// this occurs when the test suite setup has thrown an error
 			$this->out(" SETUP FAIL",'fg-red',true);
@@ -43,17 +56,18 @@ class VerboseTestResultPrinter extends PHPUnit_TextUI_ResultPrinter
 			$this->out(" FAIL",'fg-red',true);
 		}
 		else {
-			$msg = ($test instanceof PHPUnit_Framework_TestCase) ? ' OK (' . $test->getNumAssertions() . ' assertions)' : ' OK';
-			$this->out($msg,'fg-green',true);
+			
+			$numAssertions = ($test instanceof PHPUnit_Framework_TestCase) ? $test->getNumAssertions() : 1;
+		
+			if ($numAssertions > 0) {
+				$this->out(' OK (' . $numAssertions . ' assertions)','fg-green',true);
+			}
+			else {
+				$this->out(' SKIPPED (0 assertions)','fg-yellow',true);
+			}
 		}
 		
-		// copied from parent:endTest()
-		if ($test instanceof PHPUnit_Framework_TestCase) {
-			$this->numAssertions += $test->getNumAssertions();
-		}
-		else if ($test instanceof PHPUnit_Extensions_PhptTestCase) {
-			$this->numAssertions++;
-		}
+
 	}
  
 	public function startTestSuite(PHPUnit_Framework_TestSuite $suite)
@@ -68,7 +82,8 @@ class VerboseTestResultPrinter extends PHPUnit_TextUI_ResultPrinter
 				. "  \/_/     \/_/\/_/   \/_/ /_/   \/_____/   \/_____/   \/_____/   \/_____/ \n";
                                                                            
 			$this->out($header,'fg-blue',true);
-			$this->out(" - - - - - - - - - - U N I T   T E S T   R U N N E R - - - - - - - - - -\n",'fg-magenta',true);
+			$this->out(" - - - - - - - - - T E S T   A L L   T H E   T H I N G S - - - - - - - - - ",'fg-blue',true);
+			$this->out('','',true);
 			$this->headerPrinted = true;
 		}
 		
